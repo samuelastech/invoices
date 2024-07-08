12f2fb9c62e630b4607bdd621ff2b04f959d3a0d
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-07-08 13:29:14 -0300

    feat: promise timeout

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,10 @@
 import './utils/array-helpers.js';
 import { notesService as service } from "./note/note.service.js";
 import { takeUntil, debounceTime } from './utils/operators.js';
+import { timeoutPromise } from './utils/promise-helpers.js';
 
 const operation = takeUntil(3, () => {
-  service.sumItens('2143')
+  timeoutPromise(500, service.sumItens('2143'))
     .then(console.log)
     .catch(console.error)
 });
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -1,8 +1,16 @@
 export const handleStatus = async (res) => {
   return res.ok ? res.json() : Promise.reject(res.statusText);
-}
+};
 
 export const logger = (param) => {
   console.log(param);
   return param;
-}
+};
+
+export const timeoutPromise = (milliseconds, promise) => {
+  const timeout = new Promise((resolve, reject) => {
+    setTimeout(() => reject('Limite da promise excedido'), milliseconds);
+  });
+
+  return Promise.race([timeout, promise]);
+};
